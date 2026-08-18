673fb1b683adce63cba4d1e9a900dd0f997cf5a9
Guilherme Bonato <Guilhermebonato344@gmail.com>
2026-08-17 21:12:05 -0300

    Arrays de Objetos, Composição e Enums

--- Aula1.java
+++ Aula1.java
@@ -1,32 +1,34 @@
-void main (){
-    //Cria a class cardapio dentro da estrutura principal do Java
-    ItemCardapio item1 = new ItemCardapio(1, "Suco", "É um suco", 2.66, 1);
-    var item2 = new ItemCardapio(2, "Arroz com feijao", "Arroz com feijao tradicional familia brasileira", 2.66, 1);
-    //Objeto (item1), atributo(nome)
-    /*
-    item1.nome = "Suco";
-    item1.descricao = "É um suco";
-    item1.emPromocao = false;
-    item1.preco = 2.66;
-    item1.precoDesconto = 1;
-    item1.id = 1;
-    item1.categoria = 1;
-    */
-    IO.println("Descrição: " + item1.descricao + "");
-    if (item1.emPromocao == true){
-        IO.println("Esta em desconto, seu preço fica em " + item1.precoDesconto + " reaiscl");
-    } else {
-        IO.println("Não esta em promoção, seu preço original é " + item1.preco + " reais");
+void main() {
+    // Objeto (item1), atributo(nome)
+    // item1.nome = "Suco";
+    Cardapio cardapio = new Cardapio();
+    String linha = IO.readln("Digite um ID de um item do cardapio");
+    long idselecionado = Long.parseLong(linha);
+    IO.println(idselecionado);
+    // Buscando Itens no Array
+    ItemCardapio itemselecionado = cardapio.itens[((int) idselecionado) - 1];
+    for (ItemCardapio item : cardapio.itens) {
+        if (item.id == idselecionado) {
+            itemselecionado = item;
+            break;
+        }
     }
-    var porcentagemDesconto = item2.calculaPorcentagemDesconto();
+    IO.println("==Item Do Cardapio==");
+    IO.println("Nome: " + itemselecionado.nome);
+    IO.println("Em promoção: " + itemselecionado.emPromocao);
+    IO.println("Descrição: " + itemselecionado.descricao + "");
+    if (itemselecionado.emPromocao) {
+        IO.println("Esta em desconto, seu preço fica em " + itemselecionado.precoDesconto + " reaiscl");
+    } else {
+        IO.println("Não esta em promoção, seu preço original é " + itemselecionado.preco + " reais");
+    }
+    var porcentagemDesconto = itemselecionado.calculaPorcentagemDesconto();
     IO.println(porcentagemDesconto);
 
-    IO.println("Categoria: " + item2.obtemNomedaCategoria()); //Chamando um metodo
-    IO.println("______________________________");
-    IO.println("Arrays"); //Bloco de memoria, aloca memoria
+    IO.println("Categoria: " + itemselecionado.obtemNomedaCategoria()); // Chamando um metodo
+    IO.println("Arrays"); // Bloco de memoria, aloca memoria
 
-    double[] precos = new double[7]; 
-
+    double[] precos = new double[7];
     precos[0] = 2.99;
     precos[1] = 3.99;
     precos[2] = 4.99;
@@ -35,90 +37,111 @@ void main (){
     precos[5] = 8.99;
     precos[6] = 9.99;
 
-    boolean[] emPromocao = {true, false}; //Maneira literal
+    boolean[] emPromocao = { true, false }; // Maneira literal
 
     IO.println("Tamanho do array em precos: " + precos.length);
     IO.println("Tamanho do array em promoção: " + emPromocao.length);
     IO.println("O segundo item tem promoção? " + emPromocao[0]);
     IO.println("______________________________");
     IO.println("laço de repetição");
-    IO.println("While");
 
-    double totalDePreco = 0.0;
-    int i = 0;
-    while (i < precos.length) {
-        double preco = precos[i];
-        totalDePreco = totalDePreco + preco;
-        i++; // Incrementa mais 1
-    } 
-    IO.println("Total dos precos: " + totalDePreco);
-    IO.println("do While");
-    int totaldeitenspromocao = 0;
-    int j = emPromocao.length - 1;
-    do {
-        if (emPromocao[j]){
-            totaldeitenspromocao++;
-        } 
-        j--;
-    } while (j >= 0);
-    IO.println("Total de itens em promoção: " + totaldeitenspromocao);
-
-    IO.println("For");
-    double totaldeprecoscomFor = 0.0;
-    for (int h = 0; h < precos.length; h++) { //Faz tudo junto de uma vez
-        double preco = precos[h];
-        totaldeprecoscomFor += preco;
+    IO.println("Total dos precos: " + cardapio.obtemSomaDosPrecos());
+    IO.println("Total de itens em promoção: " + cardapio.obtemToalDeItensPromocao());
+    double precoLimite = 10.00;
+    for(ItemCardapio item : cardapio.itens){
+        if (item.preco <= precoLimite) {
+            IO.println("Nome do item com o preço menor que " + precoLimite + ": \n" + item.nome +" e seu preço é " + item.preco);
+            
+        }
     }
-    IO.println("Soma dos preços: " + totaldeprecoscomFor);
-    
 }
+
 // javac compila, javac teste.java vai compilar, nao vai mostrar nada no console
-// java vai rodar, java teste.java vai rodar, mostrando oq esta dentro de IO.println()
-//Classes
+// java vai rodar, java teste.java vai rodar, mostrando oq esta dentro de
+// IO.println()
+// Classes
 class ItemCardapio {
-    String nome; //Atributos
-    String descricao; //Atributos
-    boolean emPromocao; //Atributos
-    double preco; //Atributos
-    double precoDesconto; //Atributos
-    long id; //Atributos 
-    int categoria; //Atributos
-    //Linguagens orientadas a objetos
-    //Classes tem atributos e metodos
-    //Atributos são dados e os metodos são comportamento
-    //Metodo
-    double calculaPorcentagemDesconto(){
+    String nome; // Atributos
+    String descricao; // Atributos
+    boolean emPromocao; // Atributos
+    double preco; // Atributos
+    double precoDesconto; // Atributos
+    long id; // Atributos
+    CategoriaCardapio categoria; // Atributos
+    // Linguagens orientadas a objetos
+    // Classes tem atributos e metodos
+    // Atributos são dados e os metodos são comportamento
+    // Metodo
+
+    double calculaPorcentagemDesconto() {
         double calculo = (preco - precoDesconto) / preco;
         return calculo;
     }
-    
-    //Ja passa valores no new
-    //Não tem return
-    //Nome do contrutor sempre sera o nome da classe (ItemCardapio)
-    //CONTRUTOR
-    //ItemCardapio(objeto), long id(parametro), long(tipo), id(nome)
-    ItemCardapio(long id, String nome, String descricao, double preco, int precoDesconto){
-        this.id = id; //O this.id (com this) é a propriedade (o campo) que pertence ao objeto
-        this.nome = nome; //O id (sem this) é o valor que veio de fora (como um parâmetro de uma função).
+
+    // Ja passa valores no new
+    // Não tem return
+    // Nome do contrutor sempre sera o nome da classe (ItemCardapio)
+    // CONTRUTOR
+    // ItemCardapio(objeto), long id(parametro), long(tipo), id(nome)
+    ItemCardapio(long id, String nome, String descricao, double preco, int precoDesconto, CategoriaCardapio categoria) {
+        this.id = id; // O this.id (com this) é a propriedade (o campo) que pertence ao objeto
+        this.nome = nome; // O id (sem this) é o valor que veio de fora (como um parâmetro de uma função).
         this.descricao = descricao;
         this.preco = preco;
         this.precoDesconto = precoDesconto;
+        this.categoria = categoria;
     }
-    String obtemNomedaCategoria(){
-        String nomeCategoria;
-        switch (categoria) {
-        case 1:
-            nomeCategoria= "Prato Principal";
-            break;
-        case 2:
-            nomeCategoria= "Categoria: B ebidas";
-            break;
-        case 3:
-            nomeCategoria="Categoria: Sobremessas";
-        default:
-            nomeCategoria="Categoria não encontrada";
-            break;
+
+    CategoriaCardapio obtemNomedaCategoria() {
+        return categoria;
+    }
+
+    // VOID define que não tem retorno
+    void definePromocao(double precoComDesconto) {
+        emPromocao = true;
+        this.precoDesconto = precoComDesconto;
+    }
+}
+
+class Cardapio {
+    ItemCardapio[] itens;
+
+    Cardapio() {
+        // Cria a class cardapio dentro das CLASSES
+        ItemCardapio item1 = new ItemCardapio(1, "Suco", "É um suco", 2.66, 1, CategoriaCardapio.BEBIDAS);
+        var item2 = new ItemCardapio(2, "Arroz com feijao", "Arroz com feijao tradicional familia brasileira", 2.66, 1, CategoriaCardapio.PRATOS_PRINCIPAIS);
+        ItemCardapio item3 = new ItemCardapio(3, "Pizza Margherita", "Pizza com muçarela, tomate e manjericão", 15.99,
+                1, CategoriaCardapio.PRATOS_PRINCIPAIS);
+        var item4 = new ItemCardapio(4, "Suco Natural", "Suco de laranja natural e fresco", 4.50, 2, CategoriaCardapio.BEBIDAS);
+        ItemCardapio item5 = new ItemCardapio(5, "Brigadeiro", "Brigadeiro tradicional cobert com chocolate", 3.00, 3, CategoriaCardapio.SOBREMESSAS);
+        var item6 = new ItemCardapio(6, "Café Premium", "Café coado quente e aromático", 2.50, 2, CategoriaCardapio.BEBIDAS);
+        itens = new ItemCardapio[6];
+        itens[0] = item1;
+        itens[1] = item2;
+        itens[2] = item3;
+        itens[3] = item4;
+        itens[4] = item5;
+        itens[5] = item6;
+    }
+
+    double obtemSomaDosPrecos() {
+        double totalDePreco = 0.0;
+        for (ItemCardapio item : itens){
+            totalDePreco += item.preco;
         }
-        return nomeCategoria;
-    }   
+        return totalDePreco;
+    }
+    int obtemToalDeItensPromocao(){
+        int totaldeitenspromocao = 0;
+        for(ItemCardapio item : itens){
+            if (item.emPromocao){
+                totaldeitenspromocao++;
+            }
+        }
+        return totaldeitenspromocao;
+    }
+}
+
+enum CategoriaCardapio{
+    ENTRADAS, PRATOS_PRINCIPAIS, SOBREMESSAS, BEBIDAS
 }
